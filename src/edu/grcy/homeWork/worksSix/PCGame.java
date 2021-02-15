c0190bb8468caef4dd934bd76db44846f900b320
garczynskiDominik <garczynski.dominik@gmail.com>
2021-02-15 21:20:38 +0100

    Adapter- works

---
+++ src/edu/grcy/homeWork/worksSix/PCGame.java
@@ -0,0 +1,8 @@
+package edu.grcy.homeWork.worksSix;
+
+public interface PCGame {
+    String getTitle();
+    Integer getPegiAllowedAge();
+    boolean isTripleAGame();
+    Requirements getRequirements();
+}
---
+++ src/edu/grcy/homeWork/worksSix/PegiAgeRating.java
@@ -0,0 +1,5 @@
+package edu.grcy.homeWork.worksSix;
+
+public enum PegiAgeRating {
+    P3, P7, P12, P16, P18
+}
